116d2f5fae0c6ebd0f0a308762fe09b98deed2af
iamkevn <iamkevnrodriguez@gmail.com>
2020-05-06 20:29:29 -0400

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,21 +17,26 @@ end
 
 
 
-def get_japanese_emoticon(library, western_emoticon)
+def get_japanese_emoticon(yml, western_emoticon)
   # code goes here
+  library = load_library(yml)
+
   library.each do |key,value|
     if library[key][:english] == western_emoticon
        return library[key][:japanese]
     end
   end
+  "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning(library, japanese_emoticon)
+def get_english_meaning(yml, japanese_emoticon)
   # code goes here
+  library = load_library(yml)
   library.each do |key, value|
     if library[key][:japanese] == japanese_emoticon
       return key
     end
   end
+  "Sorry, that emoticon was not found"
 end
 
